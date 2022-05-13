0c328a9491f0d26df045b2befce6ea212f6c0920
lasse <77295879+lassv@users.noreply.github.com>
2022-05-13 11:42:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.get("/", (req, res) => {
 });
 app.use("/api", require("./routes/api"));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
 });
